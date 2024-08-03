f24fc167af29fccec31e33c762f23245b083f294
Surya <pawansurya1995@gmail.com>
2024-08-03 14:18:51 +0530

    sync

---
+++ closers.js
@@ -0,0 +1,15 @@
+function x(){
+    let a = 7;
+    function y() {
+        console.log(a)
+    }
+    y();
+}
+x();
+
+if(true){
+    let a = 10;
+    let b =20;
+    var c = 100;
+}
+console.log(c)
---
+++ temporalDeadZone.js
@@ -0,0 +1,3 @@
+console.log(a) // when the vaeriable is temproral dead zone you can access it 
+let a = 10;
+var c = 100;
