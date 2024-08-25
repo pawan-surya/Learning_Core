8c07f566354f784be523d631b6bec39056cb9036
Surya <pawansurya1995@gmail.com>
2024-08-25 14:51:28 +0530

    modify

--- ReverseWordsinaString.js
+++ ReverseWordsinaString.js
@@ -6,12 +6,52 @@ function reverseWords(str){
    
 }
 
-function reverseWords(str) {
+function reverseWords1(str) {
     let arr = str.trim().split(/\s+/); // Split by one or more spaces
     return arr.reverse().join(" ");
   }
   
   // Example usage
-  let resp1 = reverseWords("  Hello  world, this is  a  test  ");
+  let resp1 = reverseWords2("  Hello  world, this is  a  test  ");
+  console.log(resp); // Output: "test a is this world, Hello"
+  
+
+  function reverseWords3(str) {
+    let words = [];
+    let word = '';
+    
+    // Traverse the string to extract words
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ' ') {
+        // If a space is encountered and word is not empty, push it to words array
+        if (word.length > 0) {
+          words.push(word);
+          word = '';
+        }
+      } else {
+        // Build the current word
+        word += str[i];
+      }
+    }
+    
+    // Push the last word if there's any left
+    if (word.length > 0) {
+      words.push(word);
+    }
+    
+    // Construct the reversed string
+    let result = '';
+    for (let i = words.length - 1; i >= 0; i--) {
+      result += words[i];
+      if (i > 0) {
+        result += ' '; // Add space between words
+      }
+    }
+    
+    return result;
+  }
+  
+  // Example usage
+  let resp2 = reverseWords("Hello world, this is a test");
   console.log(resp); // Output: "test a is this world, Hello"
   
